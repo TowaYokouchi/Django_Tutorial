69494e5e129003f9a92f0b6e70852bd9f716f6c7
Yokouchi Towa <fmpmfmjgod@gmail.com>
2020-11-18 19:12:42 +0900

    [Fix] SECRET_KEYの削除

--- code/mysite/mysite/settings.py
+++ code/mysite/mysite/settings.py
@@ -19,8 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ztox7@lei#28^h5&fhmxx@*cupno1f$w91&_gy-k9%l-7_%ui('
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
